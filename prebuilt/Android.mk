d7346b04079e4fa9fb24cfea4e9c02eec7b8d777
Bluekyeet <bluekyeet@bluedmc.xyz>
2025-12-27 10:05:34 +1300

    small refactor

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -1,17 +1,13 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
-    LOCAL_MODULE := prebuilt
+    LOCAL_MODULE := miui_prebuilt
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := ETC
     LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+    LOCAL_HOST_REQUIRED_MODULES := depmod
     LOCAL_POST_INSTALL_CMD += \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/lib/firmware; \
-        cp -rf $(LOCAL_PATH)/lib/firmware/* $(TARGET_RECOVERY_ROOT_OUT)/lib/firmware/;
+        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor; \
+        cp -rf $(LOCAL_PATH)/vendor $(TARGET_RECOVERY_ROOT_OUT)/; \
+        echo "Calling depmod on miui_prebuilt"; \
+        $(DEPMOD) -b $(TARGET_RECOVERY_ROOT_OUT)/vendor 1.1;
 include $(BUILD_PHONY_PACKAGE)
