83e30b5ba5964552904d9e16be3dcea21860719b
Bluekyeet <bluekyeet@bluedmc.xyz>
2025-12-27 13:55:18 +1300

    Small refactor

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -1,19 +1,17 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-    LOCAL_MODULE := miui_prebuilt
+    LOCAL_MODULE := prebuilt
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := ETC
     LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
-    LOCAL_HOST_REQUIRED_MODULES := depmod
     LOCAL_POST_INSTALL_CMD += \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor; \
-        cp -rf $(LOCAL_PATH)/vendor $(TARGET_RECOVERY_ROOT_OUT)/; \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware; \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware_mnt/image; \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/odm/firmware/p16u; \
-        cp -a $(LOCAL_PATH)/vendor/firmware/* $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/; \
-        cp -a $(LOCAL_PATH)/vendor/firmware/* $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware_mnt/image/; \
-        cp -a $(LOCAL_PATH)/vendor/firmware/* $(TARGET_RECOVERY_ROOT_OUT)/odm/firmware/p16u/; \
-        echo "Calling depmod on miui_prebuilt"; \
-        $(DEPMOD) -b $(TARGET_RECOVERY_ROOT_OUT)/vendor 1.1;
+        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/lib/firmware; \
+        cp -rf $(LOCAL_PATH)/lib/firmware/* $(TARGET_RECOVERY_ROOT_OUT)/lib/firmware/;
 include $(BUILD_PHONY_PACKAGE)
